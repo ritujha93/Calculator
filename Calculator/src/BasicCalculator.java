b0d9cf7d14af49d899cf4a2865e758460d199133
ritu jha <ritu.ja@sap.com>
2019-03-11 15:16:48 +0530

    added basicCalculator

---
+++ Calculator/src/BasicCalculator.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author I502650
+ *
+ */
+public class BasicCalculator {
+
+	/**
+	 * 
+	 */
+	public BasicCalculator() {
+		System.out.println("Hello!!, this is a constructor");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("main class");
+
+	}
+
+}
